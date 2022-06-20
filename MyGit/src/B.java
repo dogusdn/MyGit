0dd711eb0c60735f9a71bf31e5dc138e6e85ff6b
DOHYEONWOO <dogusdn@gmail.com>
2022-06-20 16:43:38 +0900

    MyGit first Commit!

---
+++ MyGit/src/B.java
@@ -0,0 +1,7 @@
+
+public class B {
+	public void sayHello() {
+		System.out.println("Hello This is B.java File");
+	}
+	
+}
---
+++ MyGit/src/C.java
@@ -0,0 +1,7 @@
+
+public class C {
+	public void sayHello() {
+		System.out.println("Hello This is C.java File");
+	}
+	
+}
